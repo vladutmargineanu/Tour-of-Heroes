3cacf4afc615b52953098d64f3dfbc39078e2f93
Vladut Margineanu <vladut.margineanu@gmail.com>
2021-11-30 20:08:01 +0200

    Display a List

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,6 +9,7 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
@@ -16,6 +18,12 @@ export class HeroesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+   this.selectedHero = hero;
   }
 
 }
